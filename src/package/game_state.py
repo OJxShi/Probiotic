3c21fd01e08e82ee92ee7d0492a0f29d6f2bfe05
bangowang <olivia.jx.shi@gmail.com>
2024-07-21 12:41:07 -0400

    Game State Switching

--- src/package/game_state.py
+++ src/package/game_state.py
@@ -1,6 +1,4 @@
 import pygame
-from pygame.locals import *
-from config import *
 
 pygame.init()
 pygame.font.init()
@@ -19,9 +17,18 @@ class GameState():
     def update(self):
         pass
         
-    def draw(self):
+    def draw(self,screen:pygame.Surface):
         pass
-        
+
+    def change_state(self,next_state):
+        self.next_state = next_state
+    
+    def get_next_state(self):
+        if self.next_state == None:
+            return self
+        else:
+            return self.next_state
+
     def exit(self):
         active_keys.clear()
 
